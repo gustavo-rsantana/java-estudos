5a632e07ac41f44019c35069f5ffad02d58457cf
Gustavo Santana <gustavo.rsantana@hotmail.com>
2026-07-02 15:59:05 -0300

    adicionando os metodos adicionar livro e buscar libro por isbn

--- Fundamentos_Java/Projetos/SistemaBiblioteca/SistemaBiblioteca/src/com/gustavo/biblioteca/service/BibliotecaService.java
+++ Fundamentos_Java/Projetos/SistemaBiblioteca/SistemaBiblioteca/src/com/gustavo/biblioteca/service/BibliotecaService.java
@@ -10,27 +10,37 @@ import java.util.*;
 public class BibliotecaService {
     private List<Livro> livros;
     private Map<String, Livro> livrosPorIsnb;
-    private Map<String, Usuario> usuarioPorMatricula;
+    private Map<String, Usuario> usuariosPorMatricula;
     private List<Emprestimo> emprestimos;
 
     public BibliotecaService() {
         this.livros = new ArrayList<>();
         this.livrosPorIsnb = new HashMap<>();
-        this.usuarioPorMatricula = new HashMap<>();
+        this.usuariosPorMatricula = new HashMap<>();
         this.emprestimos = new ArrayList<>();
     }
 
 
-    public void cadastrarLivro(Livro livro) {};
-    public void cadastrarUsuario(Usuario usuario) {};
-    public void emprestarLivro(Emprestimo emprestimo) {};
-    public void devolverLivro(String isnb) {};
+    public void cadastrarLivro(Livro livro) {
+        if (buscarLivro(livro.getIsbn()).isEmpty()) {
+            livros.add(livro);
+            livrosPorIsnb.put(livro.getIsbn(), livro);
+        } else {
+            System.out.println("Este Livro Ja esta cadastrado.");
+        }
+    }
+    public void cadastrarUsuario(Usuario usuario) {}
+    public void emprestarLivro(Emprestimo emprestimo) {}
+    public void devolverLivro(String isbn) {}
+
     public Optional<Livro> buscarLivro(String isbn) {
-        return Optional.empty();
-    };
+        var livroSelecionado = livros.get(Integer.parseInt(isbn));
+        
+        return Optional.ofNullable(livroSelecionado);
+    }
     public List<Livro> listarLivrosDisponiveis() {
         return new ArrayList<>();
-    };
+    }
 
 
 
