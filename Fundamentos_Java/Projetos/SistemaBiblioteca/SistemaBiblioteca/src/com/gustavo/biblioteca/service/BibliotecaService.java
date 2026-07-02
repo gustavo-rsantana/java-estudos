bc1648bedf90ff613c52b1d582756c2eb59bb83b
Gustavo Santana <gustavo.rsantana@hotmail.com>
2026-07-02 14:42:48 -0300

    Fix: correção de assinaturas e nome de atributos

--- Fundamentos_Java/Projetos/SistemaBiblioteca/SistemaBiblioteca/src/com/gustavo/biblioteca/service/BibliotecaService.java
+++ Fundamentos_Java/Projetos/SistemaBiblioteca/SistemaBiblioteca/src/com/gustavo/biblioteca/service/BibliotecaService.java
@@ -4,29 +4,33 @@ import com.gustavo.biblioteca.model.Emprestimo;
 import com.gustavo.biblioteca.model.Livro;
 import com.gustavo.biblioteca.model.Usuario;
 
+import java.time.LocalDate;
 import java.util.*;
 
 public class BibliotecaService {
     private List<Livro> livros;
-    private Map<String, Livro> livroPorIsnb;
+    private Map<String, Livro> livrosPorIsnb;
     private Map<String, Usuario> usuarioPorMatricula;
     private List<Emprestimo> emprestimos;
 
     public BibliotecaService() {
         this.livros = new ArrayList<>();
-        this.livroPorIsnb = new HashMap<>();
+        this.livrosPorIsnb = new HashMap<>();
         this.usuarioPorMatricula = new HashMap<>();
         this.emprestimos = new ArrayList<>();
     }
 
 
-    public void cadastrarLivro() {};
-    public void cadastrarUsuario() {};
-    public void emprestarLivro() {};
-    public void devolverLivro() {};
-    public Optional<Livro> buscarLivro() {};
-    public List<Livro> listarLivrosDisponiveis() {};
-    public void adicionarMulta() {};
+    public void cadastrarLivro(Livro livro) {};
+    public void cadastrarUsuario(Usuario usuario) {};
+    public void emprestarLivro(Emprestimo emprestimo) {};
+    public void devolverLivro(String isnb) {};
+    public Optional<Livro> buscarLivro(String isbn) {
+        return Optional.empty();
+    };
+    public List<Livro> listarLivrosDisponiveis() {
+        return new ArrayList<>();
+    };
 
 
 
